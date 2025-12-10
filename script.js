51bc82a6e27ff3c730d792387d208d7296e7ad4d
sanudarusara <sanudarusara@gmail.com>
2025-12-10 16:33:09 +0530

    Upload files

---
+++ script.js
@@ -0,0 +1,11 @@
+function explore(){
+    document.getElementById("explore").style.visibility = "visible";
+}
+
+function back(){
+    location.reload();
+}
+
+function booknow(){
+    document.getElementById("booknow").style.visibility = "visible";
+}
